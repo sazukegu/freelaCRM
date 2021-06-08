335d87a6dd048d24408e79f97b6a069146b1fe47
Gustavo <gu.mmeneses@gmail.com>
2021-06-07 23:03:22 -0300

    About all i could do in my spare time after class

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -3,4 +3,8 @@ module TicketsHelper
     number_of_days = (Date.today - date.to_date).to_i
     number_of_days == 0 ? "Today" : "#{number_of_days} days ago"
   end
+
+  def last_activity(ticket)
+    ticket.comments.any? ? humanized_date(ticket.comments.last.updated_at) : humanized_date(ticket.updated_at)
+  end
 end
