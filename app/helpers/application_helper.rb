5302d5a601d48f71cc22d77f4e740de5fcfaf67a
Gustavo <gu.mmeneses@gmail.com>
2021-06-11 23:48:56 -0300

    :sparkles: app_controller and user_profile_pic helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def humanized_date(date)
     date.strftime("%B %d, %Y")
   end
+
+  def user_avatar(user, size=40)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      gravatar_image_url(user.email, size: size)
+    end
+  end
 end
